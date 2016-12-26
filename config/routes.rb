bb7cb63bad424ef3e36c96ce89637c8242fc27a7
Lucy Conklin <lucy.conklin@gmail.com>
2016-12-26 00:06:45 -0700

    Fix root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+  
   resources :users, only: [:new, :show, :create] do
     resources :recipes, only: [:new, :create, :index]
   end
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
